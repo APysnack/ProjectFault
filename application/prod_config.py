428cf96920befdd91d4c3351bbba2bafb840793c
APysnack <60242065+APysnack@users.noreply.github.com>
2023-09-24 14:25:32 -0400

    Last mods

--- application/prod_config.py
+++ application/prod_config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 path = '/project-fault/prod/'
 
-def get_parameter(client, parameter_name):
-    return client.get_parameter(Name=f'{path}{parameter_name}', WithDecryption=False)['Parameter']['Value']
+def get_parameter(client, parameter_name, use_decryption):
+    return client.get_parameter(Name=f'{path}{parameter_name}', WithDecryption=use_decryption)['Parameter']['Value']
 
 class Config:
     client = boto3.client('ssm', region_name='us-east-1')
 
-    SECRET_KEY = get_parameter(client, 'SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = get_parameter(client, 'SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = get_parameter(client, 'SECRET_KEY', True)
+    SQLALCHEMY_DATABASE_URI = get_parameter(client, 'SQLALCHEMY_DATABASE_URI', False)
 
     UPLOAD_FOLDER = basedir + '/static/audio/audio-files'
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3', 'wav'}

191caecf677aef7267dce8a6a6cf165bf03ef29f
APysnack <60242065+APysnack@users.noreply.github.com>
2023-09-26 10:00:09 -0400

    db stuff

--- application/prod_config.py
+++ application/prod_config.py
@@ -11,8 +11,17 @@ def get_parameter(client, parameter_name, use_decryption):
 class Config:
     client = boto3.client('ssm', region_name='us-east-1')
 
+    rds_endpoint = get_parameter(client, 'RDS_ENDPOINT', False)
+    rds_username = get_parameter(client, 'RDS_USER_NAME', False)
+    rds_password = get_parameter(client, 'RDS_PASSWORD', True)
+    rds_port = get_parameter(client, 'RDS_PORT', False)
+    rds_db_name = get_parameter(client, 'RDS_DB_NAME', False)
+
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{rds_username}:{rds_password}@{rds_endpoint}:{rds_port}/{rds_db_name}"
+
+
     SECRET_KEY = get_parameter(client, 'SECRET_KEY', True)
-    SQLALCHEMY_DATABASE_URI = get_parameter(client, 'SQLALCHEMY_DATABASE_URI', False)
+
 
     UPLOAD_FOLDER = basedir + '/static/audio/audio-files'
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3', 'wav'}
---
+++ init_db.py
@@ -0,0 +1,11 @@
+from application import create_app, db, bcrypt
+from application.models import User
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+    hashed_pw = bcrypt.generate_password_hash("password").decode("utf-8")
+    user = User(username='admin', email='email@foo.com', password=hashed_pw)
+    db.session.add(user)
+    db.session.commit()
